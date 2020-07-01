bd91d19cc5f5c4959c27aa6a66bba380642ef0da
elfrez <frezphone@gmail.com>
2020-07-01 17:48:54 -0400

    Granvia Platform

---
+++ story_content/user.js
@@ -0,0 +1,202 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6HwKHM4aKH1":
+        Script1();
+        break;
+      case "5YjrgQY9qlS":
+        Script2();
+        break;
+      case "5hmK66h3J0c":
+        Script3();
+        break;
+      case "61KnwMaztXE":
+        Script4();
+        break;
+      case "5yjxXrjHXJV":
+        Script5();
+        break;
+      case "6JWVEnnaAJA":
+        Script6();
+        break;
+      case "6CHvmEUhcih":
+        Script7();
+        break;
+      case "6U0wZ6FXPL5":
+        Script8();
+        break;
+      case "6ShtAqXK7Qp":
+        Script9();
+        break;
+      case "6UflAlG38gH":
+        Script10();
+        break;
+      case "5kbND74Qgkx":
+        Script11();
+        break;
+      case "5wePv5lvhoU":
+        Script12();
+        break;
+      case "68rLtnaA36d":
+        Script13();
+        break;
+      case "67dBRLpEaOx":
+        Script14();
+        break;
+      case "5cAfNb6T0uz":
+        Script15();
+        break;
+      case "6lzOAXwztXX":
+        Script16();
+        break;
+      case "5XDBa5vhk55":
+        Script17();
+        break;
+      case "6ZQ5Bmtb5Di":
+        Script18();
+        break;
+      case "6KAcSCsrfB1":
+        Script19();
+        break;
+      case "5bmYxSVSa2I":
+        Script20();
+        break;
+      case "5mOWAHPDHmx":
+        Script21();
+        break;
+      case "6EfiwfnWCJx":
+        Script22();
+        break;
+      case "5hXIUx4HyAD":
+        Script23();
+        break;
+      case "6ep41fFYajH":
+        Script24();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script2()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script3()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script4()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script5()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script6()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script7()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script8()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script9()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script10()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script11()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script12()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script13()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script14()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script15()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script16()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script17()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script18()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script19()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script20()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script21()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+
+player.SetVar("modulo_completado",1);
+publicar_resultado("3");
+}
+
+function Script23()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
+function Script24()
+{
+  window.location.href = "../mecanica_avanzada.html";
+}
+
